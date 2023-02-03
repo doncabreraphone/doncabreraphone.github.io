de323329d805d7fc2366a7f44712549a33bf9276
Javier Cabrera <javier@cabrerabrothers.com>
2023-02-03 10:11:11 -0300

    lazy load

--- javascript.js
+++ javascript.js
@@ -290,8 +290,45 @@ function typeWriter() {
                 });
             })(i);
         }
-        
-        
+
+        if($("#destinations").length) {
+            let images = $("img");
+            let imagesToLazyLoad = images.filter(function() {
+              return this.src.endsWith(".webp");
+            });
+          
+            let imagesInView = function() {
+              let windowHeight = $(window).height();
+              let windowTopPosition = $(window).scrollTop();
+              let windowBottomPosition = windowTopPosition + windowHeight;
+          
+              imagesToLazyLoad.each(function() {
+                let image = $(this);
+                let imageTopPosition = image.offset().top;
+                let imageBottomPosition = imageTopPosition + image.height();
+          
+                if (
+                  imageBottomPosition >= windowTopPosition &&
+                  imageTopPosition <= windowBottomPosition
+                ) {
+                  image.attr("src", image.data("src"));
+                  imagesToLazyLoad = imagesToLazyLoad.not(image);
+                }
+              });
+            };
+          
+            imagesToLazyLoad.each(function() {
+              let image = $(this);
+              image.attr("data-src", image.attr("src"));
+              image.removeAttr("src");
+            });
+          
+            $(window).scroll(function() {
+              imagesInView();
+            });
+          
+            imagesInView();
+        }
 
       });
 
