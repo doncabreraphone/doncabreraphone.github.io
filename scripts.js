7cdd14aee818362a1e4579aee61eb8fc23b4edf7
Javier Cabrera <javier@cabrerabrothers.com>
2023-08-15 18:04:58 -0300

    alkanos home

--- scripts.js
+++ scripts.js
@@ -151,13 +151,16 @@
     }
 
     function onPlayer2Ready(event) {
-        // Set up time intervals for player 2
         var intervals = [
-            { start: 22, end: 27 },
-            { start: 32, end: 34 },
-            { start: 40, end: 48 },
-            { start: 64, end: 67 },
-        // Add more intervals as needed
+            // { start: 22, end: 27 },
+            // { start: 32, end: 34 },
+            // { start: 40, end: 48 },
+            // { start: 64, end: 67 },
+            { start: 12, end: 13 },
+            { start: 4, end: 7 },
+            { start: 25, end: 30 },
+            { start: 42, end: 45 },
+            { start: 156, end: 159 },
         ];
 
         var currentInterval = 0;
@@ -684,8 +687,72 @@ let isAnimating = false;
     //   });
   
 
-            
-    });
+
+
+    if ($('.image-ticker').length) {
+    window.onload = function(){
+        var windowWidth = window.innerWidth;
+        var elementWidth = 290;
+        var numElToAppend = Math.ceil(windowWidth/elementWidth) * 1;
+        var parentElSelector = '.image-ticker ul';
+        var elSelector = 'li';
+        var parentEl = document.querySelector(parentElSelector);
+        var itemEls = parentEl.querySelectorAll(elSelector);
+        if(numElToAppend > itemEls.length){
+          var numElToAppendFraction = numElToAppend/itemEls.length;
+          var fullRepeat = ~~numElToAppendFraction;
+          var partialRepeat = Math.ceil((numElToAppendFraction-fullRepeat)*10);
+        }
+        for(var i = 0; i < fullRepeat; i++){
+          for(var j = 0; j<itemEls.length;j++){
+            parentEl.appendChild(itemEls[j].cloneNode(true));  
+          }
+        }
+        var itemsToAppend = Array.prototype.slice.call(itemEls, 0, partialRepeat);
+        for(var i = 0; i< itemsToAppend.length; i++){
+          console.log(itemsToAppend[i]);
+          parentEl.appendChild(itemsToAppend[i].cloneNode(true));
+        }
       
+      }
+
+    }
+
+
+
+
+    $(window).on("scroll", function() {
+        if ($('.slide-up').length) {
+            $(".slide-up").each(function(index) {
+                const slideDiv = $(this);
+                const scrollPosition = $(window).scrollTop();
+                const elementPosition = $(this).offset().top;
+                if (elementPosition - scrollPosition < $(window).height()) {
+                  slideDiv.addClass("active");
+                } else {
+                  slideDiv.removeClass("active");
+                }
+              });        
+        }
+
+        if ($('.image-container').length) {
+            $(".image-container").each(function(index) {
+                const slideImage = $(this).find("img");
+                const scrollPosition = $(window).scrollTop();
+                const elementPosition = $(this).offset().top;
+                if (elementPosition - scrollPosition < $(window).height()) {
+                slideImage.addClass("active");
+                } else {
+                slideImage.removeClass("active");
+                }
+            });
+        }
+    });
+
+       
+    
+
+
+    });  
       
       
